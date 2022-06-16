e3245eb2a55d021d796e84e89d3dfc4594c727e0
han <rkddkwl1364@gmail.com>
2022-06-17 01:29:25 +0900

    developing

--- pages/login.tsx
+++ pages/login.tsx
@@ -45,14 +45,16 @@ function Login({ providers }: ProviderProps) {
         className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
       />
       <div className="flex space-x-2">
-        <button
-          className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5
-            transition-all hover:border-2"
-          onClick={() => signIn(providers?.google?.id, { callbackUrl: "/" })}
-        >
-          {providers?.google?.name}
-        </button>
-        <button
+        <div>
+          <button
+            className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5
+          transition-all hover:border-2"
+            onClick={() => signIn(providers?.google?.id, { callbackUrl: "/" })}
+          >
+            {providers?.google?.name}
+          </button>
+        </div>
+        {/* <button
           className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5
             transition-all hover:border-2"
           onClick={() => signIn(providers?.kakao?.id, { callbackUrl: "/" })}
@@ -65,7 +67,7 @@ function Login({ providers }: ProviderProps) {
           onClick={() => signIn(providers?.naver?.id, { callbackUrl: "/" })}
         >
           {providers?.naver?.name}
-        </button>
+        </button> */}
       </div>
     </div>
   );
