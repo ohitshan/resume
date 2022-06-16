d0a163368ff9170f34b3a2a60731fbdf612ae1d8
han <rkddkwl1364@gmail.com>
2022-06-16 18:03:15 +0900

    developing

--- pages/login.tsx
+++ pages/login.tsx
@@ -12,7 +12,7 @@ interface providerForm {
 }
 interface ProviderProps {
   providers: {
-    github: providerForm;
+    kakao: providerForm;
     google: providerForm;
     naver: providerForm;
   };
